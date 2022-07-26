345ea1f2a8be86bde2533c198cdad9d495ce7975
Chandrikabogem <chandrika.bogem@xcubelabs.com>
2022-07-26 15:14:41 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 import Aboutus from './components/Aboutus';
+import {Routes, Route} from "react-router-dom"
+
+
+import SecondTheme from './components/SecondTheme';
+import FixedNav from './components/FixedNav';
+import Menu from './components/Menu';
+import Reservation from './components/Reservation';
+import News from './components/News';
+import Gallery from './components/Gallery';
+import Footer from './components/Footer'
+
+
 
 function App() {
   return (
     <div className="App">
-      <Aboutus/>
+     <FixedNav/>
+       <Routes>
+      <Route path="/" element={<SecondTheme/>}/>
+
+       <Route path="aboutus" element={<Aboutus/>}/> 
+       <Route path="menu" element={<Menu/>}/> 
+       <Route path="reservation" element={<Reservation/>}/> 
+       <Route path="news" element={<News/>}/> 
+       <Route path="gallery" element={<Gallery/>}/> 
+       </Routes>   
+       <Footer/> 
     </div>
   );
 }
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Gallery.scss'
 
+
 const Gallery = () => {
     return (
         <div>
